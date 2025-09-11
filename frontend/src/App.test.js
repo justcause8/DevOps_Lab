a393847d1c423330e66325344f171cac418dad67
Дмитрий Зверев <dmzverev8@gmail.com>
2025-09-11 22:22:10 +0800

    fix: Убрал коментарий

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 // Автотест 1 тестовый
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 test('renders learn react link', () => {
   
   render(<App />);
