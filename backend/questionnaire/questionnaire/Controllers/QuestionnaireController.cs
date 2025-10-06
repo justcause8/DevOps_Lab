18f3ddc1733e7f930cc2cf8a81f402ffc1d21522
Justcause <dmzverev8@gmai.com>
2025-10-06 14:55:12 +0800

    fix/test-fix: убрал отступ

--- backend/questionnaire/questionnaire/Controllers/QuestionnaireController.cs
+++ backend/questionnaire/questionnaire/Controllers/QuestionnaireController.cs
@@ -21,7 +21,6 @@ namespace questionnaire.questionnaire.Controllers
             _context = context;
         }
 
-
         // Получение данных текущего пользователя
         [HttpGet("current")]
         [Authorize]
